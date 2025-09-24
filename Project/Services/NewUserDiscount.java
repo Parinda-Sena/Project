dc6f7844d20d74b8cb9cea7453b25b8a6744ca66
Chaliptul <apinutchaya.w@ku.th>
2025-09-24 16:45:03 +0700

    NewUserDiscount.java

--- Project/Services/NewUserDiscount.java
+++ Project/Services/NewUserDiscount.java
@@ -1,6 +1,17 @@
 package Services;
-public class NewUserDiscount {
+public class NewUserDiscount extends Discount  {
     /*
      * ส่วนลด 10% สำหรับลูกค้าใหม่
      */
+    private final double discountAmount;
+
+    public NewUserDiscount(double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    public double applyDiscount(double price) {
+        return price - discountAmount; // ลดราคาตามจำนวนที่กำหนด
+    }
 }
+
