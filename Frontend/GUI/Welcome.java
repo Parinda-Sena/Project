6c2caeb6596f393e3a471790d8e3ed61b8f5f53e
SutthiphonThiansap <sutthiphon.t@ku.th>
2025-10-05 05:03:25 +0700

    Add files via upload

---
+++ Frontend/GUI/Welcome.java
@@ -0,0 +1,39 @@
+package GUI;
+import java.awt.*;
+import javax.swing.*;
+
+public class Welcome {
+    public Welcome(){
+
+        JFrame Welcome = new JFrame("Meow Ordering");
+        Welcome.setLayout(null);
+        Welcome.setSize(400, 700);
+        Welcome.setIconImage(new ImageIcon("C:\\Users\\sutti\\OneDrive\\Desktop\\Frontend\\Photo\\Logo\\LogoJFrame.png").getImage());
+        Welcome.getContentPane().setBackground(new Color(255, 255, 204));
+        Welcome.setLocationRelativeTo(null);
+        
+
+        ImageIcon icon = new ImageIcon("C:\\Users\\sutti\\OneDrive\\Desktop\\Frontend\\Photo\\Logo\\LogoWelcome.png");
+        Image img = icon.getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+        JLabel label = new JLabel(new ImageIcon(img));
+        label.setBounds(41, 10, 300, 300);
+        Welcome.add(label);
+        
+        JLabel welcome = new JLabel("Welcome");
+        welcome.setBounds(120, 200, 200, 200);
+        welcome.setFont(new Font("Arial", Font.BOLD, 36));
+        Welcome.add(welcome);
+
+
+        JButton Login = new JButton("Login");
+        Login.setBounds(95, 375, 200, 50);
+        Welcome.add(Login);
+
+        JButton Signin = new JButton("Sign Up");
+        Signin.setBounds(95, 450, 200, 50);
+        Welcome.add(Signin);
+
+        Welcome.setVisible(true);
+        Welcome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ Frontend/Test.java
@@ -0,0 +1,9 @@
+import GUI.*;
+
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        new Signin();
+    }
+}
